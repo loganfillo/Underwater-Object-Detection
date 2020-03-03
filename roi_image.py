41a5dfd2e9d75cffed90095329be3c3554d2c701
Hello <logan.fillo@gmail.com>
2020-03-03 11:55:19 -0800

    added naive convex hull classifier

--- roi_image.py
+++ roi_image.py
@@ -1,6 +1,10 @@
 import cv2 as cv
 import numpy as np
 
+# Free parameters of the system 
+IMG_RESIZE_SCALE = 1/3
+NUM_CLUSTERS = 5
+
 def preprocess(src):
     # Apply CLAHE and Gaussian on each RGB channel then downsize
     clahe = cv.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
@@ -10,7 +14,7 @@ def preprocess(src):
     bgr[1] = cv.GaussianBlur(clahe.apply(bgr[1]), kernel, 0) 
     bgr[2] = cv.GaussianBlur(clahe.apply(bgr[2]), kernel, 0) 
     src = cv.merge(bgr)
-    src = cv.resize(src, (int(src.shape[1]/3), int(src.shape[0]/3)), cv.INTER_CUBIC )
+    src = cv.resize(src, (int(src.shape[1]*IMG_RESIZE_SCALE), int(src.shape[0]*IMG_RESIZE_SCALE)), cv.INTER_CUBIC )
     return src
 
 def gradient(src):
@@ -26,72 +30,106 @@ def gradient(src):
     return grad
 
 def cluster(src):
-    # Compute gradient of grayscale version of source
+
+    # Compute gradient on saturation channel of image (seems to have best response to pole)
     hsv = cv.cvtColor(src, cv.COLOR_BGR2HSV)
     grad = gradient(hsv[:,:,1])
 
-    # Create features where each feature vector is [hue, sat, val, grad_magnitude, color_mask]
+    # Create color mask on HSV image
     err = 30
     upper = (97+err, 56+err, 158+err)
     lower = (97-err, 56-err, 158-err)
     color_mask = cv.inRange(hsv, lower, upper)
-    lab = cv.cvtColor(src, cv.COLOR_BGR2LAB)
+
+    # Create features where each feature vector is [hue, sat, val, grad_magnitude, color_mask]
     features = cv.merge([hsv, grad, color_mask ]).reshape((hsv.shape[0]*hsv.shape[1], 5)) 
     features_float = np.float32(features)
 
     # K Means segmentation, cluster pixels using k means then segment image using grayscale values
-    num_clusters = 4
+    num_clusters = NUM_CLUSTERS
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 1.0)
     ret,labels,center=cv.kmeans(features_float,num_clusters,None,criteria,3,cv.KMEANS_RANDOM_CENTERS)
     labels = labels.reshape(hsv.shape[0], hsv.shape[1])
     labelled_image = (labels.astype(np.float)/(num_clusters-1))
     labelled_image = (labelled_image*255).astype(np.uint8)
-    return grad,  labelled_image
+
+    return color_mask,  labelled_image
 
 def morphological(src):
     # Dilation then erosion to smooth segmentation
-    # TODO: figure out if these are needed at all
     kernel = np.ones((3,3), np.uint8)
     dilated = cv.dilate(src, kernel, iterations=1)
     eroded = cv.erode(dilated, kernel, iterations=1)
     return eroded
 
-def convex_hulls(src):
+def convex_hulls(src, orig):
+
     # Define convex hull around contours of each cluster
     drawing = np.zeros((src.shape[0], src.shape[1], 3), np.uint8)
+
+    # Scale raw image to be the same as the segmented image on which the convex hulls are computed
+    orig = cv.resize(orig, (int(orig.shape[1]*IMG_RESIZE_SCALE), int(orig.shape[0]*IMG_RESIZE_SCALE)), cv.INTER_CUBIC)
+
+    # Search over the binary images associated to each cluster
     for i in np.unique(src):
+
+        # Create binary image
         bin = np.where(src!= i, 0, 255).astype(np.uint8)
+
+        # If a cluster takes up more than half the screen, it most likely does not contain the poles, skip to next cluster
         if np.sum(bin != 0) >= (bin.shape[0]*bin.shape[1]/2):
-            continue 
+            continue
+
         # First find contours in the image
         contours, hierarchy = cv.findContours(bin, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         hulls = []
-        # Create a convhex hull around each connected contour
+
+        # Create a convex hull around each connected contour
         for j in range(len(contours)):
             hulls.append(cv.convexHull(contours[j], False))
-        big_hulls = []
-        # Get the hulls which are within some range (< 1/4 of image, > 1/5000 of image)
+        pole_hulls = []
+
         for hull in hulls:
             con = hull.reshape(hull.shape[0],2).transpose()
             x = con[0] 
             y = con[1] 
             con = np.array([x, y]).transpose().reshape((-1,1,2))
-            if (np.max(x)-np.min(x))*(np.max(y)-np.min(y)) > src.shape[0]*src.shape[1]/5000 and (np.max(x)-np.min(x))*(np.max(y)-np.min(y)) < src.shape[0]*src.shape[1]/4:
-                blank = np.zeros((src.shape[0], src.shape[1]), np.uint8)
-                cv.polylines(blank, [con], True, 255)
-                #This displays each hull seperately on a blank image 
-                # cv.imshow('Segmented', blank)
+            
+            # Get the hulls whose area is within some range (< 1/5 of image, > 1/5000 of image for example)
+            im_size = src.shape[0]*src.shape[1]
+            if (np.max(x)-np.min(x))*(np.max(y)-np.min(y)) > im_size/5000 and (np.max(x)-np.min(x))*(np.max(y)-np.min(y)) < im_size/5:
+                
+                #This displays each good size hull and coprresponding fitted ellipse seperately on a blank image 
+                # blank = np.zeros((src.shape[0]*2, src.shape[1]*2,3), np.uint8)
+                # cv.polylines(blank, [con], True, (0,0,255))
+                # ellipse = cv.fitEllipse(con)
+                # cv.ellipse(blank, ellipse,(0,255,0), 1)
+                # cv.imshow('Convex Hull', blank)
                 # cv.waitKey(0)
-                big_hulls.append(hull)
-        # Display all big hulls 
-        for j in range(len(big_hulls)):
-            cv.drawContours(drawing, big_hulls, j, (0,0,255), 1, 8)
-    return drawing
+
+                # Sometime fitEllipse doesn't work on a convex hull, we need dummy values
+                angle = 0
+                MA = 1
+                ma = 1
+                try:
+                    (x,y),(MA,ma),angle = cv.fitEllipse(con)
+                except:
+                    pass
+                cosAngle = np.abs(np.cos(angle*np.pi/180))
+
+                # Only add hull to pole hulls if it is reasonably a vertically oriented rectangle
+                if  (cosAngle < 1.2) and (cosAngle > 0.98) and (MA/ma < 0.25):
+                    pole_hulls.append(hull)
+
+        # Display all pole hulls on original image
+        for j in range(len(pole_hulls)):
+            cv.drawContours(orig,pole_hulls, j, (0,0,255), 1, 8)
+    return orig
 
 def segmentation(src):
     src = preprocess(src)
-    hsv, src = cluster(src)
-    return hsv, src
+    other, src = cluster(src)
+    return other, src
 
 ##################################
 # Video
@@ -104,7 +142,7 @@ while(cap.isOpened()):
   if ret == True:
     other, seg = segmentation(frame)
     # seg = morphological(seg)
-    con = convex_hulls(seg)
+    con = convex_hulls(seg, frame)
     cv.imshow('Processed', preprocess(frame))
     cv.imshow('other', other)
     cv.imshow('Segmented', seg)
@@ -120,11 +158,11 @@ cv.destroyAllWindows()
 # Single Image
 ##################################
 # src = cv.imread('./imgs/medium.jpg',1)
-# hsv, seg = segmentation(src)
-# seg = morphological(seg)
-# con = convex_hulls(seg)
+# other, seg = segmentation(src)
+# # seg = morphological(seg)
+# con = convex_hulls(seg, src)
 # cv.imshow('Source', preprocess(src))
 # cv.imshow('Segmented', seg)
-# cv.imshow('color_mask', hsv)
+# cv.imshow('color_mask', other)
 # cv.imshow('Convex hulls', con)
 # cv.waitKey(0)
